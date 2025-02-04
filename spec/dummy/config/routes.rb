8de4789f9ac5f435a5cba0b0cbca562e38282d38
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2025-02-04 14:32:21 +0900

    Fix routes for test

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'file' => 'application#file'
-  get 'redirect' => redirect('/test/index.html')
-  get '/test/redirect_to_other_host' => redirect(host: 'sub.localhost', path: '/test/index.html')
-  get 'not_modified' => 'application#not_modified'
-  get 'gone' => 'application#gone'
-  get 'internal_server_error' => 'application#internal_server_error'
-  get 'image_file_jp' => 'application#image_file_jp'
-  get 'image_file_en' => 'application#image_file_en'
+  get 'file', to: 'application#file'
+  get 'redirect', to: redirect('/test/index.html')
+  get '/test/redirect_to_other_host', to: redirect(host: 'sub.localhost', path: '/test/index.html')
+  get 'not_modified', to: 'application#not_modified'
+  get 'gone', to: 'application#gone'
+  get 'internal_server_error', to: 'application#internal_server_error'
+  get 'image_file_jp', to: 'application#image_file_jp'
+  get 'image_file_en', to: 'application#image_file_en'
 end
